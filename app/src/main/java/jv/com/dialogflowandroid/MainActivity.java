0bef01591317382026ac62e3bf75a7a0d27ecb67
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-04-08 11:32:04 +0530

    showing response in text view....still not connected to intent

--- app/src/main/java/jv/com/dialogflowandroid/MainActivity.java
+++ app/src/main/java/jv/com/dialogflowandroid/MainActivity.java
@@ -3,6 +3,11 @@ package jv.com.dialogflowandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.JsonElement;
+
+import java.util.Map;
 
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
@@ -10,7 +15,7 @@ import ai.api.model.AIError;
 import ai.api.model.AIResponse;
 import ai.api.ui.AIButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
 //    AIConfiguration config = new AIConfiguration("ce6a85a61d7c4ef69b71f80eef0ebfb6",
 //            AIConfiguration.SupportedLanguages.English,
@@ -20,15 +25,24 @@ public class MainActivity extends AppCompatActivity {
 
     AIButton aiButton;
 
+    TextView resultTextView;
+
+    private AIService aiService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultTextView = findViewById(R.id.resultTextView);
+
 
         AIConfiguration config = new AIConfiguration("ce6a85a61d7c4ef69b71f80eef0ebfb6",
                 AIConfiguration.SupportedLanguages.English,
                 AIConfiguration.RecognitionEngine.System);
+
+
+        aiService = AIService.getService(this, config);
 
 
         aiButton = (AIButton) findViewById(R.id.micButton);
@@ -42,6 +56,24 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("ApiAi", "onResult");
                         // TODO process response here
                         Log.d("result",result.toString());
+
+
+
+
+                // Get parameters
+                String parameterString = "";
+                if (result.getResult().getParameters() != null && !result.getResult().getParameters().isEmpty()) {
+                    for (final Map.Entry<String, JsonElement> entry : result.getResult().getParameters().entrySet()) {
+                        parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+                    }
+                }
+
+                // Show results in TextView.
+                resultTextView.setText("Query:" + result.getResult().getResolvedQuery() +
+                        "\nAction: " + result.getResult().getAction() +
+                        "\nParameters: " + parameterString);
+
+
 //                    }
 //                });
             }
